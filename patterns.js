af9d3cc43cd5f84c1ce9f8344267442f2e672d20
Matthew Crittle <legendsmith.au@gmail.com>
2016-11-05 08:01:56 +1100

    TOOLTIPS ENGAGE! Styles => gaspstyle.css. Added penalty cap, moved UI features around
    Tooltips on High res statloss, and penalty cap.  Also prepped
    patterns.js for an upcoming feature.

--- patterns.js
+++ patterns.js
@@ -19,4 +19,26 @@ pctx.lineTo(x1-offset,y1);
 pctx.moveTo(x0+offset,y0);
 pctx.lineTo(x1+offset,y1);
 pctx.stroke();
+
+var p2 = document.createElement("canvas")
+p2.width=16;
+p2.height=16;
+var pctx2=p2.getContext('2d');
+
+var x0=-2;
+var x1=18;
+var y0=-2;
+var y1=18;
+var offset=16;
+
+pctx2.strokeStyle = "#77DD66";
+pctx2.lineWidth=4;
+pctx2.beginPath();
+pctx2.moveTo(x0,y0);
+pctx2.lineTo(x1,y1);
+pctx2.moveTo(x0-offset,y0);
+pctx2.lineTo(x1-offset,y1);
+pctx2.moveTo(x0+offset,y0);
+pctx2.lineTo(x1+offset,y1);
+pctx2.stroke();
 console.log("Patterns successfully created")
